8d0992b104f2fa04a691e8682e2744389b17bddd
Howard Fraser <howardjfraser@gmail.com>
2015-11-23 08:16:07 +1100

    More tests.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -23,7 +23,6 @@ module SessionsHelper
   end
 
   def current_user
-    # was @current_user ||= User.find_by(id: session[:user_id])
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
--- app/models/user.rb
+++ app/models/user.rb
@@ -27,7 +27,7 @@ class User < ActiveRecord::Base
   def authenticated?(remember_token)
     return false if remember_digest.nil?
 
-    # TODO - ?
+    # TODO - ???
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
