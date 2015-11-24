9a9d192e6587930bba57053d39bfd03899a776c8
Howard Fraser <howardjfraser@gmail.com>
2015-11-25 07:59:18 +1100

    Comments.

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,7 +31,7 @@ class User < ActiveRecord::Base
   end
 
   def User.digest(string)
-    # TODO - ??? cost stuff
+    # TODO - cost stuff???
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
