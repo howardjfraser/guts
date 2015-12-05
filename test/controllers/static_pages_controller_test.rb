398691f2e0b1b0ef12e29affa3e16d54bf9c1464
Howard Fraser <howardjfraser@gmail.com>
2015-12-05 12:09:23 +1100

    Decruft pt 1.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -8,21 +8,4 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_select "title", "Questions?"
   end
 
-  test "should get help" do
-    get :help
-    assert_response :success
-    assert_select "title", "Help | Questions?"
-  end
-
-  test "should get about" do
-    get :about
-    assert_response :success
-    assert_select "title", "About | Questions?"
-  end
-
-  test "should get contact" do
-    get :contact
-    assert_response :success
-    assert_select "title", "Contact | Questions?"
-  end
 end
