677c1520158c7d4c797a9edfba25ed9583f44505
Howard Fraser <howardjfraser@gmail.com>
2015-12-07 07:49:25 +1100

    Tidy up.

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -9,7 +9,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "layout links not logged in" do
     get root_path
     assert_template 'static_pages/home'
-    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", root_path
     assert_select "a[href=?]", signup_path
     assert_select "a[href=?]", login_path
   end
@@ -18,7 +18,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     log_in_as @user
     follow_redirect!
     assert_template 'users/show'
-    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", root_path
     assert_select "a[href=?]", users_path
     assert_select "a[href=?]", user_path(@user)
     assert_select "a[href=?]", edit_user_path(@user)
