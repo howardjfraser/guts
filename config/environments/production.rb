d16c6f49cc193a3ba3e34c5a55cec92cfc15b4ed
Howard Fraser <howardjfraser@gmail.com>
2015-12-11 09:05:01 +1100

    Fix domain for Sendgrid.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'http://lit-oasis-4797.herokuapp.com'
+  host = 'http://enigmatic-springs-5637.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
 
   ActionMailer::Base.smtp_settings = {
