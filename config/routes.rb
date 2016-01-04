746b3c1d704ebcc896b17e46f13e458c8db92a86
Howard Fraser <howardjfraser@gmail.com>
2016-01-04 14:42:15 +1100

    Change route format.

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
 
-  root 'static_pages#home'
+ root 'static_pages#home'
 
-  # replace with new trial inc company name
-  get    'signup' => 'users#new' # dupe
+ # replace with new trial inc company name
+ get 'signup' => 'users#new' # dupe
 
-  get    'login'  => 'sessions#new'
-  post   'login'  => 'sessions#create'
-  delete 'logout' => 'sessions#destroy'
+ get 'login' => 'sessions#new'
+ post 'login' => 'sessions#create'
+ delete 'logout' => 'sessions#destroy'
 
-  resources :users
-  resources :account_activations, only: [:edit]
-  resources :password_resets, only: [:new, :create, :edit, :update]
+ resources :users
+ resources :account_activations, only: [:edit]
+ resources :password_resets, except: [:show, :destroy]
 
 end
