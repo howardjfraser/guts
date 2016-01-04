aa4dedaab79ccf4e5a97b694871e8819d127192e
Howard Fraser <howardjfraser@gmail.com>
2016-01-04 14:37:57 +1100

    Reword.

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   root 'static_pages#home'
 
   # replace with new trial inc company name
-  get 'signup' => 'users#new' # dupe
+  get    'signup' => 'users#new' # dupe
 
-  get 'login' => 'sessions#new'
-  post 'login' => 'sessions#create'
+  get    'login'  => 'sessions#new'
+  post   'login'  => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
 
   resources :users
