69195dca7b1fe455738f5fd9f06d03bc04f45e0f
Howard Fraser <howardjfraser@gmail.com>
2016-01-14 09:14:18 +1100

    Remove unused route.

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,6 @@ Rails.application.routes.draw do
 
  resources :users
  resources :account_activations, only: [:edit]
- resources :password_resets, except: [:show, :destroy]
+ resources :password_resets, except: [:index, :show, :destroy]
 
 end
