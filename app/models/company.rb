900577424aceb9c029dbbb3701eeea54e61cab63
Howard Fraser <howardjfraser@gmail.com>
2016-01-15 10:41:45 +1100

    Comments.

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,12 @@
 class Company < ActiveRecord::Base
   has_many :users
 
+  # has owner? (or is_owner on user?)
+
+  # trial expiry date
+
+  # state [trial, subscribed, expired] (or derive?)
+
   validates :name, presence: true, length: {maximum: 50}
   validates :users, presence: true
 
