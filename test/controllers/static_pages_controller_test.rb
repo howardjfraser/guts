5e8fad57590d56a1ab62d6de868af50252662b14
Howard Fraser <howardjfraser@gmail.com>
2016-01-27 07:39:09 +1100

    Name.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,7 +5,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "welcome is accessible without login" do
     get :welcome
     assert_response :success
-    assert_select "title", "Enigmatic Springs"
+    assert_select "title", "Guts"
     assert_select "h1", "Welcome"
   end
 
