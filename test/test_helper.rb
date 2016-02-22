9879124cc2f18118eddcddf81cff14e9c84d77fe
Howard Fraser <howardjfraser@gmail.com>
2016-02-22 16:18:01 +1100

    More rubocop.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,57 +8,59 @@ Minitest::Reporters.use!
 require 'simplecov'
 SimpleCov.start 'rails'
 
-class ActiveSupport::TestCase
-  fixtures :all
+module ActiveSupport
+  class TestCase
+    fixtures :all
 
-  def setup
-    @howard = users(:howard)
-    @brent = users(:brent)
-    @michael = users(:michael)
-    @gareth = users(:gareth)
-    @tim = users(:tim)
-    @ricky = users(:ricky)
+    def setup
+      @howard = users(:howard)
+      @brent = users(:brent)
+      @michael = users(:michael)
+      @gareth = users(:gareth)
+      @tim = users(:tim)
+      @ricky = users(:ricky)
 
-    @keith = users(:keith)
-    @keith.create_reset_digest
+      @keith = users(:keith)
+      @keith.create_reset_digest
 
-    @hogg = companies(:wernham_hogg)
-    @mifflin = companies(:dunder_mifflin)
-  end
-
-  def is_logged_in?
-    !session[:user_id].nil?
-  end
-
-  def is_logged_in_as?(user)
-    session[:user_id] == user.id
-  end
-
-  def log_in_as(user, options = {})
-    password = options[:password] || 'password'
-    remember_me = options[:remember_me] || '1'
-    if integration_test?
-      post_via_redirect login_path, session: { email: user.email, password: password, remember_me: remember_me }
-    else
-      session[:user_id] = user.id
+      @hogg = companies(:wernham_hogg)
+      @mifflin = companies(:dunder_mifflin)
     end
-  end
 
-  private
+    def is_logged_in?
+      !session[:user_id].nil?
+    end
 
-  def integration_test?
-    defined?(post_via_redirect)
-  end
+    def is_logged_in_as?(user)
+      session[:user_id] == user.id
+    end
 
-  def check_access(expected, user = nil)
-    log_in_as user unless user.nil?
-    yield
-    assert_response expected
-  end
+    def log_in_as(user, options = {})
+      password = options[:password] || 'password'
+      remember_me = options[:remember_me] || '1'
+      if integration_test?
+        post_via_redirect login_path, session: { email: user.email, password: password, remember_me: remember_me }
+      else
+        session[:user_id] = user.id
+      end
+    end
 
-  def check_redirect(expected, user = nil)
-    log_in_as user unless user.nil?
-    yield
-    assert_redirected_to expected
+    private
+
+    def integration_test?
+      defined?(post_via_redirect)
+    end
+
+    def check_access(expected, user = nil)
+      log_in_as user unless user.nil?
+      yield
+      assert_response expected
+    end
+
+    def check_redirect(expected, user = nil)
+      log_in_as user unless user.nil?
+      yield
+      assert_redirected_to expected
+    end
   end
 end
