14aeecb1ab1fef58f52a766ed4f71da53240df95
Howard Fraser <howardjfraser@gmail.com>
2016-05-19 14:10:52 +1000

    Comments.

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,15 @@ Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
   resource :signup, only: [:new, :create]
 
-  resources :activations, only: [:edit, :update]
   resources :companies, only: [:index, :show, :edit, :update, :destroy]
+  resources :updates, only: [:index, :create]
+  resources :users
+
+  # group under users?
+  resources :activations, only: [:edit, :update]
   resources :password_changes, only: [:edit, :update]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :root_users, only: [:update]
-  resources :updates, only: [:index, :create]
-  resources :users
 
   # TODO: use resources?
   post 'invitations/resend/:id' => 'invitations#resend', as: 'resend_invitation'
