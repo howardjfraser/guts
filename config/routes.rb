0b04a68d70a3b0a9c2e8d7dae01e59dc3ce3d3de
Howard Fraser <howardjfraser@gmail.com>
2016-05-19 13:45:01 +1000

    Format.

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,19 @@
 Rails.application.routes.draw do
   root 'updates#index'
 
-  resource :signup, only: [:new, :create]
   resource :session, only: [:new, :create, :destroy]
-
-  resources :companies, except: [:new, :create]
-
-  resources :users
-  resources :root_users, only: [:update]
+  resource :signup, only: [:new, :create]
 
   resources :activations, only: [:edit, :update]
-  resources :password_resets, only: [:new, :create, :edit, :update]
+  resources :companies, only: [:index, :show, :edit, :update, :destroy]
   resources :password_changes, only: [:edit, :update]
+  resources :password_resets, only: [:new, :create, :edit, :update]
+  resources :root_users, only: [:update]
+  resources :updates, only: [:index, :create]
+  resources :users
 
   # TODO: use resources?
   post 'invitations/resend/:id' => 'invitations#resend', as: 'resend_invitation'
-
-  resources :updates, only: [:index, :create]
 
   get 'signup', to: redirect('/signup/new')
 end
