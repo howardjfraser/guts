557156c53710f6d86ed2700299a17c4818f7f5bf
Howard Fraser <howardjfraser@gmail.com>
2016-05-31 08:05:15 +1000

    Split out error helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,26 +2,4 @@ module ApplicationHelper
   def full_title(page_title)
     page_title + ' | Guts'
   end
-
-  def error_class(object, field)
-    'has-danger' if error? object, field
-  end
-
-  def error_message(object, field)
-    error object.errors.full_messages_for(field)[0] if error? object, field
-  end
-
-  def custom_error_message(object, field, label)
-    error "#{label} #{object.errors.get(field)[0]}" if error? object, field
-  end
-
-  private
-
-  def error?(object, field)
-    object.errors[field].count > 0
-  end
-
-  def error(msg)
-    content_tag(:span, msg, class: 'text-danger')
-  end
 end
---
+++ app/helpers/error_helper.rb
@@ -0,0 +1,23 @@
+module ErrorHelper
+  def error_class(object, field)
+    'has-danger' if error? object, field
+  end
+
+  def error_message(object, field)
+    error object.errors.full_messages_for(field)[0] if error? object, field
+  end
+
+  def custom_error_message(object, field, label)
+    error "#{label} #{object.errors.get(field)[0]}" if error? object, field
+  end
+
+  private
+
+  def error?(object, field)
+    object.errors[field].count > 0
+  end
+
+  def error(msg)
+    content_tag(:span, msg, class: 'text-danger')
+  end
+end
